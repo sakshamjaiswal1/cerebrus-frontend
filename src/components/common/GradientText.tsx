13f644157d832ca7d3face78d4322fcb6ab4b3a2
Lucky6400 <luckyjain7247@gmail.com>
2025-09-12 00:53:18 +0530

    fix: ensure GradientText component is fully responsive with max width

--- src/components/common/GradientText.tsx
+++ src/components/common/GradientText.tsx
@@ -1,7 +1,7 @@
 const GradientText = ({ text, className }: { text: string, className?: string }) => {
   return (
     <h1
-      className={`text-4xl font-semibold bg-clip-text text-transparent text-left mb-4 w-max ${className}`}
+      className={`text-4xl font-semibold bg-clip-text text-transparent text-left mb-4 w-max max-w-full ${className}`}
       style={{
         backgroundImage:
           "linear-gradient(90.09deg, #1A323C 0%, #A5F5ED 90%, #A5F5ED 100%)",
