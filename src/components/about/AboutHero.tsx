6a014efd270e937617114e59f98fee0b1befbda2
Lucky6400 <luckyjain7247@gmail.com>
2025-09-18 01:37:31 +0530

    style: update AboutHero text styling for improved readability

--- src/components/about/AboutHero.tsx
+++ src/components/about/AboutHero.tsx
@@ -5,7 +5,7 @@ const AboutHero = () => {
     <section className="w-full py-8 sm:py-12 lg:py-16">
       <div className=" mx-auto">
         <GradientText text="About Us" className="mb-0" />
-        <p className="text-sm sm:text-base leading-relaxed max-w-4xl text-primary opacity-70">
+        <p className="text-sm sm:text-base lg:text-xl leading-relaxed w-full text-primary font-[300]">
           Built under the expertise of Griphic, Cerebrus is not just a product of HR technology, but of a core belief that the best path for innovation is through strengthening human judgment.
         </p>
       </div>
