23752c203d55863918afc52dd2cfbc760470cadd
Saksham Jaiswal <75566599+sakshamjaiswal1@users.noreply.github.com>
2025-09-21 10:52:31 +0530

    svg-fix

--- src/components/common/header/DesktopNavigation.tsx
+++ src/components/common/header/DesktopNavigation.tsx
@@ -58,7 +58,9 @@ const DesktopNavigation = () => {
               viewBox="0 0 12 12"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="transition-transform duration-200 hover:rotate-180"
+              className={`transition-transform duration-200 hover:rotate-180 ${
+                isDropdownOpen ? "rotate-180" : ""
+              }`}
             >
               <path
                 d="M3 4.5L6 7.5L9 4.5"
