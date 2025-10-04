f38dfeb8f1ffd232ff5fb30e99cba1973cb5ff9f
Lucky6400 <luckyjain7247@gmail.com>
2025-10-05 03:34:42 +0530

    style: enhance BlogCTA and AnimatedButton components with updated typography, border styles, and new gradient border option for improved visual appeal

--- src/components/blog/BlogCTA.tsx
+++ src/components/blog/BlogCTA.tsx
@@ -4,19 +4,19 @@ const BlogCTA = () => {
   return (
     <section className="w-full py-8 sm:py-12 lg:py-16">
       <div className="mx-auto px-4 sm:px-6 lg:px-0">
-        <div className="bg-brand-bg rounded-2xl p-4 sm:p-6 lg:p-8 border border-gray-300">
+        <div className="bg-brand-bg rounded-2xl p-4 sm:p-6 lg:p-8 border border-[#E1E3D8]">
           <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between gap-4 sm:gap-6 lg:gap-8">
             
             {/* Left Side - Text Content */}
-            <div className="w-full lg:w-11/12">
-              <h2 className="text-lg sm:text-xl lg:text-5xl text-primary leading-tight mb-2 sm:mb-4">
+            <div className="w-full">
+              <h2 className="text-lg sm:text-xl lg:text-4xl font-medium text-primary leading-tight mb-2 sm:mb-4">
                 Business Leader of the Year' at the 33rd World HRD Congress
               </h2>
             </div>
             
             {/* Right Side - Button */}
             <div className="w-full lg:w-1/3 flex justify-start lg:justify-end">
-              <AnimatedButton text="Read more" className="!border !border-primary !bg-transparent !text-primary" />
+              <AnimatedButton text="Read more" className="!border-none !text-primary !font-semibold !py-1.5 !px-4 hover:!text-white" gradientBorder />
             </div>
           </div>
         </div>
